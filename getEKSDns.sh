5c22d89e6b22781497f546db3f7e9e478828764b
Pranay Shah <pshah@tibco.com>
2021-08-11 23:45:21 -0700

    added script to get EKS DNS

---
+++ getEKSDns.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Prepare script with correct switches
+set +x
+set -e
+set -u
+set -o pipefail
+
+CLUSTER_NAME=${1:-FAKE}
+region=${2:-us-west-2}
+
+
+echo "Input is : CLUSTER_NAME=${CLUSTER_NAME}"
+echo "Input is : region=${region}"
+
+VPC_ID=$(aws eks describe-cluster --name "$CLUSTER_NAME" --query "cluster.resourcesVpcConfig.vpcId" --output text --region "$region")
+echo "VPC ID for cluster $CLUSTER_NAME is [$VPC_ID]"
+DNS_NAME=$(aws elbv2 describe-load-balancers --region "$region" | jq ".LoadBalancers[] | select(.VpcId==\"${VPC_ID}\")" | jq -r .DNSName)
+export DNS_NAME=$DNS_NAME
+
+echo "EKS DNS is : $DNS_NAME"
