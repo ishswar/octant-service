4e08442fadcb81508c89cb543c48238132346427
Pranay Shah <pshah@tibco.com>
2021-08-15 11:30:59 -0700

    added script to get EKS DNS

--- build-email-body.sh
+++ build-email-body.sh
@@ -18,7 +18,9 @@ sed -i -e "s!JOB_URL!$BUILD_URL!g" email.html
 sed -i -e "s!JOB_ID!$BUILD_NUMBER!g" email.html
 sed -i -e "s!PARAMTER_VERSION!$product_version!g" email.html
 
-SWEGO_URL=${SWEGO_URL// } 
+echo "SWEGO_URL is before [$SWEGO_URL]"
+
+SWEGO_URL=${SWEGO_URL// }
 
 echo "SWEGO_URL is [$SWEGO_URL]"
 
