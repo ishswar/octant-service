421dc488df6b8e63edcf7e63d02da14c64e0a2c3
Pranay Shah <pshah@tibco.com>
2021-08-15 20:14:14 -0700

    added script to get EKS DNS

--- getEKSDns.sh
+++ getEKSDns.sh
@@ -5,23 +5,28 @@ set +x
 set -e
 set -u
 
+##########################################################################################################
+### Helper script - it will use AWS CLI installed on machine
+### Givne EKS cluster name and AWS region this script will find DNS of ELBV2 created for this cluster ####
+### Output will be sotred in path $OUTPUT_FILE (i.e jenkins/terraform/dns-$CLUSTER_NAME.txt)
+##########################################################################################################
+
 CLUSTER_NAME=${1:-FAKE}
 region=${2:-us-west-2}
+OUTPUT_FILE=${3:-jenkins/terraform/dns-$CLUSTER_NAME.txt}
 
 
 echo "Input is : CLUSTER_NAME=${CLUSTER_NAME}"
 echo "Input is : region=${region}"
+echo "Input is : OUTPUT_FILE=${OUTPUT_FILE}"
 
 VPC_ID=$(aws eks describe-cluster --name "$CLUSTER_NAME" --query "cluster.resourcesVpcConfig.vpcId" --output text --region "$region")
 echo "VPC ID for cluster $CLUSTER_NAME is [$VPC_ID]"
 DNS_NAME=$(aws elbv2 describe-load-balancers --region "$region" --output json | jq ".LoadBalancers[] | select(.VpcId==\"${VPC_ID}\")" | jq -r .DNSName)
-export DNS_NAME=$DNS_NAME
-
-echo "EKS DNS is : [$DNS_NAME]"
 
 echo "Cleaning up DNS_NAME value $DNS_NAME"
 DNS_NAME=$(echo "$DNS_NAME"|tr '\n' ' ') # Remove new line 
 DNS_NAME=${DNS_NAME%% } # Remove trailing spaces 
 echo "After Cleaning up DNS_NAME value [$DNS_NAME]"
 echo "Writing DNS Entry into file"
-echo "$DNS_NAME" > jenkins/terraform/dns-$CLUSTER_NAME.txt
+echo "$DNS_NAME" > "$OUTPUT_FILE"
