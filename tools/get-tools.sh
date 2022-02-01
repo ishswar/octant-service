7643dac3a6f56ac65b53c4e187d0c0787014f161
Pranay Shah <pshah@tibco.com>
2022-02-01 09:57:36 -0800

    added so file for top

--- tools/get-tools.sh
+++ tools/get-tools.sh
@@ -20,11 +20,13 @@ curl -s https://raw.githubusercontent.com/ishswar/octant-service/master/tools/wg
 curl -s https://raw.githubusercontent.com/ishswar/octant-service/master/tools/nano -O
 curl -s https://raw.githubusercontent.com/ishswar/octant-service/master/tools/nslookup -O
 curl -s https://raw.githubusercontent.com/ishswar/octant-service/master/tools/top -O
+curl -s https://raw.githubusercontent.com/ishswar/octant-service/master/tools/libprocps.so.7 -O
 chmod +x ping
 chmod +x wget
 chmod +x nano
 chmod +x nslookup
 chmod +x top
+cp libprocps.so.7 /lib64/libprocps.so.7
 
-echo "Done"
+echo "Done copying tools to /opt/webfocus/logs/tools"
 ls -la
